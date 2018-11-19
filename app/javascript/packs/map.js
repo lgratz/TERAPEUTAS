0ec1f8b53076b1e281171c9df7379ceff79f01c5
Felippe <felippe@meubiz.com.br>
2018-11-19 16:40:09 -0200

    ajustes no mapa2

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -10,20 +10,21 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
   if (markers.length === 0) {
     map.setZoom(2);
   } else if (markers.length === 1) {
-    map.setCenter(markers[0].lat, markers[0].lng);
-    map.setZoom(14);
+    // map.setCenter(markers[0].lat, markers[0].lng);
+    // map.setZoom(14);
   } else {
     map.fitLatLngBounds(markers);
   }
 
   if(navigator.geolocation){
+
          // call this function to get the location
     navigator.geolocation.getCurrentPosition(function(position) {
          // the results return as a lat lng,
          //which we will put them into a variable to use in Google's Geocoder
       var Mypos = {
-        Mylat: position.coords.latitude,
-        Mylng: position.coords.longitude
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
       };
       console.log(Mypos)
 
@@ -35,8 +36,10 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
         lat: position.coords.latitude,
         lng: position.coords.longitude,
         infoWindow: info,
-        icon: "https://i1.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?resize=40%2C40&quality=100&ssl=1"
+        icon: "http://icons-for-free.com/free-icons/png/48/1891016.png"
       });
+    map.setZoom(14);
+    map.panTo(Mypos);
     });
    }
 
