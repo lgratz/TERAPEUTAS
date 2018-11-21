1333eefac599b5f78d789aebd3fb0df0c90e4cef
Livia Gratz <livia.gratz@gmail.com>
2018-11-21 17:52:44 -0200

    Add appointments ux and Calendar settings

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,11 +1,5 @@
 class CalendarsController < ApplicationController
-  def create
-    @therapist = Therapist.find(params[:therapist_id])
-    @calendar = Calendar.new
-    @calendar.therapist = @therapist
-    @calendar.save
-  end
-
   def edit
+    @calendar = current_user.therapist.calendar
   end
 end
