f906e3fc64d351f6a57fbdf89158c8e0bf5d9786
andres fernandez <andres.webd@gmail.com>
2018-11-21 17:22:41 -0200

    Added javascript transition to the navbar. Added logo in the home-page.

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,11 +1,11 @@
 function initUpdateNavbarOnScroll() {
-  const navbar = document.querySelector('.navbar-wagon');
+  const navbar = document.querySelector('.navbar-home');
   if (navbar) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight) {
-        navbar.classList.add('navbar-wagon');
+        navbar.classList.add('navbar-wagon-gray');
       } else {
-        navbar.classList.add('navbar-wagon');
+        navbar.classList.remove('navbar-wagon-gray');
       }
     });
   }
